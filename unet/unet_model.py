057e2a0135154b78537e49c0ad891c5e918c06e4
RM Sandu <ralucam.sandu@gmail.com>
2025-03-16 20:04:05 +0100

    Update U-Net model with attention gates

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -1,5 +1,7 @@
-""" Full assembly of the parts to form the complete network """
+"""Full assembly of the parts to form the complete network"""
 
+import torch
+import torch.nn as nn
 from .unet_parts import *
 
 
@@ -16,6 +18,13 @@ class UNet(nn.Module):
         self.down3 = Down(256, 512)
         factor = 2 if bilinear else 1
         self.down4 = Down(512, 1024 // factor)
+
+        # Add attention gates
+        self.attention1 = AttentionGate(F_g=512 // factor, F_l=512, F_int=256)
+        self.attention2 = AttentionGate(F_g=256 // factor, F_l=256, F_int=128)
+        self.attention3 = AttentionGate(F_g=128 // factor, F_l=128, F_int=64)
+        self.attention4 = AttentionGate(F_g=64, F_l=64, F_int=32)
+
         self.up1 = Up(1024, 512 // factor, bilinear)
         self.up2 = Up(512, 256 // factor, bilinear)
         self.up3 = Up(256, 128 // factor, bilinear)
@@ -28,25 +37,53 @@ class UNet(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+
+        # Apply attention gates on skip connections
+        x4_att = self.attention1(g=x5, x=x4)
+        x = self.up1(x5, x4_att)
+
+        x3_att = self.attention2(g=x, x=x3)
+        x = self.up2(x, x3_att)
+
+        x2_att = self.attention3(g=x, x=x2)
+        x = self.up3(x, x2_att)
+
+        x1_att = self.attention4(g=x, x=x1)
+        x = self.up4(x, x1_att)
+
         logits = self.outc(x)
         if self.n_classes == 1:
             return torch.sigmoid(logits)  # Binary segmentation
         else:
             return torch.softmax(logits, dim=1)  # Multi-class segmentation
-        # return torch.sigmoid(logits)
 
     def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
+        """Enable gradient checkpointing for memory efficiency"""
+
+        def create_custom_forward(module):
+            def custom_forward(*inputs):
+                return module(*inputs)
+
+            return custom_forward
+
+        # Apply checkpointing to each module
+        self.inc = torch.utils.checkpoint.checkpoint_sequential([self.inc], 1, self.inc)
+        self.down1 = torch.utils.checkpoint.checkpoint_sequential(
+            [self.down1], 1, self.down1
+        )
+        self.down2 = torch.utils.checkpoint.checkpoint_sequential(
+            [self.down2], 1, self.down2
+        )
+        self.down3 = torch.utils.checkpoint.checkpoint_sequential(
+            [self.down3], 1, self.down3
+        )
+        self.down4 = torch.utils.checkpoint.checkpoint_sequential(
+            [self.down4], 1, self.down4
+        )
+        self.up1 = torch.utils.checkpoint.checkpoint_sequential([self.up1], 1, self.up1)
+        self.up2 = torch.utils.checkpoint.checkpoint_sequential([self.up2], 1, self.up2)
+        self.up3 = torch.utils.checkpoint.checkpoint_sequential([self.up3], 1, self.up3)
+        self.up4 = torch.utils.checkpoint.checkpoint_sequential([self.up4], 1, self.up4)
+        self.outc = torch.utils.checkpoint.checkpoint_sequential(
+            [self.outc], 1, self.outc
+        )
--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -1,4 +1,4 @@
-""" Parts of the U-Net model """
+"""Parts of the U-Net model"""
 
 import torch
 import torch.nn as nn
@@ -19,7 +19,6 @@ class DoubleConv(nn.Module):
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            
         )
 
     def forward(self, x):
@@ -62,9 +61,6 @@ class Up(nn.Module):
         diffX = x2.size()[3] - x1.size()[3]
 
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
@@ -76,3 +72,40 @@ class OutConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
+
+
+class AttentionGate(nn.Module):
+    def __init__(self, F_g, F_l, F_int):
+        """
+        Attention Gate
+        Args:
+            F_g: Number of feature channels from the higher layer (gating signal)
+            F_l: Number of feature channels from the skip connection
+            F_int: Intermediate channel dimension
+        """
+        super(AttentionGate, self).__init__()
+
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int),
+        )
+
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int),
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid(),
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
